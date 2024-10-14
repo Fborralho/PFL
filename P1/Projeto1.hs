d29bfb5f49273a27ad910286a9cc7349697f3eb4
frankie02m2023 <your.email@example.com>
2024-10-14 20:24:43 +0100

    Adding functions 1 to 7 to francisco branch.

--- P1/Projeto1.hs
+++ P1/Projeto1.hs
@@ -1,6 +1,9 @@
 import qualified Data.List
 import qualified Data.Array
 import qualified Data.Bits
+import Distribution.Simple.Setup (falseArg)
+import Data.List (sortBy)
+import GHC.Exts.Heap (GenClosure(value))
 
 -- PFL 2024/2025 Practical assignment 1
 
@@ -12,29 +15,98 @@ type Distance = Int
 
 type RoadMap = [(City,City,Distance)]
 
+getFirstCity :: (City,City,Distance) -> City
+getFirstCity (city1,_,_) = city1
+
+getSecondCity :: (City,City,Distance) -> City
+getSecondCity (_,city2,_) = city2
+
+getDistance :: (City,City,Distance) -> Distance
+getDistance (_,_,distance) = distance
+
+removeRepeatedCities :: [City] -> [City]
+removeRepeatedCities [] = []
+removeRepeatedCities (x:xs)
+    | x `elem` xs  = removeRepeatedCities xs
+    | otherwise = x : removeRepeatedCities xs
+
 cities :: RoadMap -> [City]
-cities roadMap = Data.List.nub [city | (city1, city2, _) <- roadMap, city <- [city1, city2]]
+cities [] = []
+cities (x:xs) = removeRepeatedCities ([getFirstCity x, getSecondCity x] ++ cities xs)
 
 areAdjacent :: RoadMap -> City -> City -> Bool
-areAdjacent roadMap city1 city2 = any (\(c1, c2, _) -> (c1 == city1 && c2 == city2) || (c1 == city2 && c2 == city1)) roadMap
+areAdjacent roadMap city1 city2 = any cityPair roadMap
+    where
+        cityPair(a,b,_) = ((a == city1) || (a == city2)) && ((b == city1) || (b == city2))
 
 distance :: RoadMap -> City -> City -> Maybe Distance
-distance roadMap c1 c2 =
-        if areAdjacent roadMap c1 c2 
-            then Just(head [dist | (city1,city2,dist) <- roadMap, (c1 == city1 && c2 == city2 ) || (c1 == city2 && c2 == city1)])
-        else Nothing
+distance roadMap city1 city2
+    | areAdjacent roadMap city1 city2 = Just (getDistance (head (filter cityPair roadMap)))
+    | otherwise = Nothing
+  where
+    cityPair (a, b, _) = (a == city1 && b == city2) || (a == city2 && b == city1)
 
 adjacent :: RoadMap -> City -> [(City,Distance)]
-adjacent = undefined
+adjacent [] city = []
+adjacent (x:xs) city
+    | getFirstCity x == city = (getSecondCity x, getDistance x) : adjacent xs city
+    | getSecondCity x == city = (getFirstCity x, getDistance x): adjacent xs city
+    | otherwise = adjacent xs city
+
+pathExists :: RoadMap -> Path -> Bool
+pathExists [] _ = False
+pathExists _ [] = True
+pathExists _ [_] = True
+pathExists roadMap (x:xs)
+    | not (areAdjacent roadMap x (head xs)) = False
+    | otherwise = pathExists roadMap xs
+
+pathDistance' :: RoadMap -> Path -> Distance
+pathDistance' [] _ = 0
+pathDistance' _ [] = 0
+pathDistance' _ [_] = 0
+pathDistance' roadMap (x:y:xs) =
+    case distance roadMap x y of
+        Just d -> d + pathDistance' roadMap (y:xs)
 
 pathDistance :: RoadMap -> Path -> Maybe Distance
-pathDistance = undefined
+pathDistance [] _ = Nothing
+pathDistance _ [] = Just 0
+pathDistance _ [_] = Just 0
+pathDistance roadMap path
+    | not (pathExists roadMap path) = Nothing
+    | otherwise = Just (pathDistance' roadMap path)
+
+numberOfConnections :: RoadMap -> City -> Int
+numberOfConnections [] _ = 0
+numberOfConnections roadMap city = length (adjacent roadMap city)
+
+getNumberOfConnectionsList :: RoadMap -> [City] -> [(City,Int)]
+getNumberOfConnectionsList [] _ = []
+getNumberOfConnectionsList _ [] = []
+getNumberOfConnectionsList roadMap (x:xs) = (x,numberOfConnections roadMap x) : getNumberOfConnectionsList roadMap xs
+
+filterConnections :: [(City,Int)] -> Int -> [City]
+filterConnections [] _ = []
+filterConnections (x:xs) value
+    | snd x == value = fst x : filterConnections xs value
+    | otherwise = filterConnections xs value
+
 
 rome :: RoadMap -> [City]
-rome = undefined
+rome [] = []
+rome roadMap = filterConnections (getNumberOfConnectionsList roadMap (cities roadMap)) maxConnections
+    where
+        maxConnections = maximum (map snd (getNumberOfConnectionsList roadMap (cities roadMap)))
 
 isStronglyConnected :: RoadMap -> Bool
-isStronglyConnected = undefined
+isStronglyConnected [] = True
+isStronglyConnected roadMap
+    |any cityPair (getNumberOfConnectionsList roadMap (cities roadMap)) = False
+    |otherwise = True
+        where 
+            cityPair(_,connectionNumber) = connectionNumber /= length (cities roadMap) - 1
+
 
 shortestPath :: RoadMap -> City -> City -> [Path]
 shortestPath = undefined
@@ -55,4 +127,8 @@ gTest2 = [("0","1",10),("0","2",15),("0","3",20),("1","2",35),("1","3",25),("2",
 gTest3 :: RoadMap -- unconnected graph
 gTest3 = [("0","1",4),("2","3",2)]
 
+gTest4 :: RoadMap -- unconnected graph
+gTest4 = [("Porto","Lisboa",450),("Porto","Aveiro",200),("Porto","Faro",500),("Lisboa","Setubal",90),("Lisboa","Faro",210)]
 
+gTest5 :: RoadMap -- unconnected graph
+gTest5 = [("0","1", 2), ("0","2", 1), ("1", "2", 0)]
